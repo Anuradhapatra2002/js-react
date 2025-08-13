cfd17f7b7c1d01b0d16bf8c9e0a015b8f1851637
Anuradhapatra2002 <patraanuradha09@gmail.com>
2025-08-13 08:51:26 +0530

    mouse event  , keyboard event , pointer eveent

---
+++ day24 dom events/script.js
@@ -0,0 +1,18 @@
+let btn = document.querySelector("button")
+
+btn.onclick=()=>{
+    console.log("btn was clicked");
+    let a = 23;
+    a++;
+    console.log(a);
+    
+    
+
+}
+let div = document.querySelector("div")
+console.log(div);
+
+div.onmouseover=()=>{
+    console.log("you are inside div");
+    
+}
