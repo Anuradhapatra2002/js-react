ce3c2c182817393447a370a51a7e110bf8a1a58b
Anuradhapatra2002 <patraanuradha09@gmail.com>
2025-08-20 15:28:58 +0530

    import & export

---
+++ day 29 end of js/export concept in.js
@@ -0,0 +1,24 @@
+// const user = 'sai'
+// //Named export 
+// export{
+//     user
+
+// }
+
+// export{user}
+// function greet(){
+//     console.log('hello everyone');
+    
+// }
+// export{greet};
+// // console.log('export named' , user);
+
+
+//Default export
+
+//const name = 'saii pranbab'
+function sayhii(){
+    console.log("helooooooooooo");
+    
+}
+export default sayhii
---
+++ day 29 end of js/import concept in.js
@@ -0,0 +1,11 @@
+ const user = 'sai'
+//Named import
+ import { greet } from "./export concept in.js"
+greet()
+
+//Default import 
+import xyz from './export concept in.js'
+console.log(xyz);
+xyz()
+
+
